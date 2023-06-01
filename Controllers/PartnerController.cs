790ed18e4d8ccd20de9030c9440f5160df4fd3db
Karen Lieby Danion <karen_lieby@yahoo.com.ar>
2023-06-01 12:07:29 +0200

    on voit s'il marche sur vos ordis

--- Controllers/PartnerController.cs
+++ Controllers/PartnerController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using TripMeOn.BL;
 using TripMeOn.BL.interfaces;

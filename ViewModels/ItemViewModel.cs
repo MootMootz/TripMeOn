19449f3643bdfcd87aff97b8b2f999a51fe29d7a
SerdarU <s-ustundag@hotmail.com>
2023-06-08 11:05:23 +0200

    list order client ok

--- ViewModels/ItemViewModel.cs
+++ ViewModels/ItemViewModel.cs
@@ -2,9 +2,18 @@
 {
     public class ItemViewModel
     {
-        public int TourPackageId { get; set; }
+        public int? TourPackageId { get; set; }
         public string TourPackageName { get; set; }
         public double TourPackagePrice { get; set; }
+        public int? AccomodationId { get; set; }
+        public string AccomodationName { get; set; }
+        public double AccomodationPrice { get; set; }
+        public int? RestaurantId { get; set; }
+        public string RestaurantName { get; set; }
+        public double RestaurantPrice { get; set; }
+        public int? TransportId { get; set; }
+        public string TransportationType { get; set; }
+        public double TransportationPrice { get; set; }
         public int Quantity { get; set; }
         public string ImageUrl { get; set; }
     }
